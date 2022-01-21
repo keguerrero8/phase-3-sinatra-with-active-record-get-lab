94fee53380c25ac7b13601f8e8bab62e61a7ab14
Kevin Guerrero <guerrk83z@gmail.com>
2022-01-20 20:48:46 -0500

    commit tree 590b318edf87f8aa47a173ff1b80552b1e5346ce

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,44 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  # add routes
+  # get '/games' do
+  #   # get all the games from the database
+  #   games = Game.all
+  #   # return a JSON response with an array of all the game data
+  #   games.to_json
+  # end
+
+  # get '/games/:id' do
+  #   game = Game.find(params[:id])
+
+  #   # include associated reviews in the JSON response
+  #   game.to_json(include: { reviews: { include: :user } })
+  # end
+
+  
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    # get all the bakeries from the database
+    bakery = Bakery.find(params[:id])
+    # send them back as a JSON array
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.all.order(price: :desc)
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    exp_baked_good = BakedGood.all.order(price: :desc).first
+    exp_baked_good.to_json
+  end
+
 
 end
